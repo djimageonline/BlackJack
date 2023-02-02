ab923ef13c02eb3956ff63e0d27d1e10f9d338d4
Johnny Proano <jproano22@yahoo.com>
2023-02-01 22:46:58 -0500

    added conditional for basic black jack, and new variables to store state

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard + 4;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+if (sum <= 20) {
+  message = "Do you want to hit?";
+} else if (sum === 21) {
+  message = "BlackJack! You won!";
+  hasBlackJack = true;
+} else {
+  message = "Ouch, Maybe next time.  You Lost!";
+  let isAlive = false;
+}
+
+//Cash out money
+console.log(message);
